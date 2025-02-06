a6d628ea7ecb442d6627534a2def741647739f7e
Ziad Znr <ziadznr311@gmail.com>
2025-02-06 19:15:42 +0600

    continue update

--- src/models/StudentsModel.js
+++ src/models/StudentsModel.js
@@ -1,10 +1,28 @@
 const mongoose = require("mongoose")
 
 const DataSchema = mongoose.Schema({
-    Name: String,
-    Roll: String,
-    Class: String,
-    Remarks: String
+    Name: { type: String },
+    Roll: {
+        type: Number,
+        min: [6, ' minimum value 6 and maximum value 12, But supplied value is ={VALUE}'],
+        max: [12, ' minimum value 6 and maximum value 12, But supplied value is ={VALUE}']
+    },
+    Class: { type: String },
+    Remarks: String,
+    // Custom validation
+    Mobile: {
+        type: Number,
+        validate: {
+            validator: function(value) {
+                if (value.length !== 11) {
+                    return false
+                } else {
+                    return true
+                }
+            },
+            message: '11 Digit phone number required'
+        }
+    }
 })
 const StudentsModel = mongoose.model('students', DataSchema)
 module.exports = StudentsModel;
